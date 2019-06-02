c850353558f5ab5aa500b6ec7b1ca55cd080d46a
shaheennamboori <chichushaheen@gmail.com>
2019-06-02 19:57:04 +0530

    uploaded audios

--- app/src/main/java/com/dscvast/kootu/MainActivity.java
+++ app/src/main/java/com/dscvast/kootu/MainActivity.java
@@ -5,11 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView idli, dosa, upma, putt, rice, chappathi;
-    private final int[] resID = {R.raw.sond};
+    private final int[] resID = {R.raw.idli, R.raw.dosa, R.raw.upma, R.raw.puttu, R.raw.choru, R.raw.chapathi};
     private MediaPlayer mp;
 
     @Override
@@ -38,19 +37,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 playSong(0);
                 break;
             case R.id.dosa:
-                playSong(0);
+                playSong(1);
                 break;
             case R.id.upma:
-                playSong(0);
+                playSong(2);
                 break;
             case R.id.putt:
-                Toast.makeText(getApplicationContext(), "putt", Toast.LENGTH_SHORT).show();
+                playSong(3);
                 break;
             case R.id.rice:
-                Toast.makeText(getApplicationContext(), "rice", Toast.LENGTH_SHORT).show();
+                playSong(4);
                 break;
             case R.id.chappathi:
-                Toast.makeText(getApplicationContext(), "chappathi", Toast.LENGTH_SHORT).show();
+                playSong(5);
         }
     }
 
