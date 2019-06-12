c9b92113cea5c5c5e0c1760493a00e29aef154e0
Shinil M S <shinilms@gmail.com>
2019-06-12 14:56:48 +0530

    Migrate to AndroidX

--- app/src/main/java/com/dscvast/kootu/SettingsActivity.java
+++ app/src/main/java/com/dscvast/kootu/SettingsActivity.java
@@ -1,7 +1,7 @@
 package com.dscvast.kootu;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import static java.util.Objects.requireNonNull;
 
